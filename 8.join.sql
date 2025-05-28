039fd27d644ccc5a54adb8c7d14f75459df54b2b
JihyeonKim <wwwlllwwwlll92@gmail.com>
2025-05-28 20:53:43 +0900

    250528

--- 8.join.sql
+++ 8.join.sql
@@ -53,3 +53,99 @@ WHERE i.ANIMAL_ID is null;
 SELECT o.ANIMAL_ID, o.NAME
 FROM ANIMAL_OUTS o
 WHERE o.ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM ANIMAL_INS);
+
+-- union : 두 테이블의 select결과를 횡으로 결합(기본적으로 distinct 적용)
+-- union시킬때 컬럼의 개수와 컬럼의 *타입*이 같아야함
+select name, email from author union select title, content from post;
+-- union all : 중복까지 모두 포함
+select name, email from author union all select title, content from post;
+
+-- 서브쿼리 : select문 안에 또다른 select문을 서브쿼리라 한다.
+-- where절 안에 서브쿼리
+-- 한번이라도 글을 쓴 author 목록 조회
+select distinct a.* from author a inner join post p on a.id = p.author_id;
+-- null값은 in조건절에서 자동으로 제외
+select * from author where id in(select author_id from post);
+
+-- 컬럼 위치에 서브쿼리
+-- author의 email과 author별로 본인의 쓴 글의 개수를 출력
+select email, (select count(*) from post p where a.id = p.author_id) from author a;      -- -> 순서: 필요한순서임. 여기서는 메인쿼리->서브쿼리
+
+-- from절 위치에 서브쿼리
+select a.* from (select * from author where id>5) as a;
+
+-- group by 컬럼명 : 특정 컬럼으로 데이터를 그룹화 하여, 하나의 행(row)처럼 취급
+select author_id from post group by author_id;
+-- 보통 아래와 같이 집계함수와 같이 많이 사용.
+select author_id, count(*) from post group by author_id;    -- -> 주로 group by한 컬럼을 select함. 이때 count(*)은 전체데이터에 대한 *이 아니라, group by상태에 대한 *이 나옴
+
+-- 집계함수
+-- null은 count에서 제외
+select count(*) from author;
+select sum(price) from post;
+select avg(price) from post;
+-- 소수점 3번째 자리에서 반올림
+select round(avg(price), 3) from post;
+
+-- group by와 집계함수
+select author_id, count(*), sum(price) from post group by author_id;
+
+-- where와 group by  -> where의 위치는 전체데이터에 대한 필터링이므로 table 옆.
+-- 날짜별(즉, 시간제외) post 글의 개수 출력. (날짜 값이 null은 제외)
+select DATE_FORMAT(created_time, '%Y-%m-%d') as day, count(*) from post where created_time is not null group by day;
+
+-- 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
+SELECT CAR_TYPE, COUNT(*) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS LIKE '%통풍시트%' OR OPTIONS LIKE '%열선시트%' OR OPTIONS LIKE '%가죽시트%'           -- -> *WHERE조건이 복잡함...
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC;
+-- 입양 시각 구하기(1)
+SELECT DATE_FORMAT(DATETIME, '%H') AS HOUR, COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+WHERE DATE_FORMAT(DATETIME, '%H') >= '09' 
+AND DATE_FORMAT(DATETIME, '%H') < '20'
+GROUP BY HOUR
+ORDER BY HOUR;
+
+
+-- group by와 having
+-- having은 group by를 통해 나온 집계값에 대한 조건
+-- 글을 2번 이상 쓴 사람 ID 찾기
+select author_id from post group by author_id having count(*) >= 2;
+-- 동명 동물 수 찾기
+SELECT NAME, COUNT(*) AS COUNT
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+GROUP BY NAME 
+HAVING COUNT(*) >= 2
+ORDER BY NAME;
+-- 카테고리 별 도서 판매량 집계하기
+SELECT b.CATEGORY, SUM(bs.SALES) AS TOTAL_SALES
+FROM BOOK b INNER JOIN BOOK_SALES bs ON b.BOOK_ID = bs.BOOK_ID
+WHERE date_format(bs.SALES_DATE, '%Y-%m') = '2022-01'
+GROUP BY b.CATEGORY
+ORDER BY b.CATEGORY;
+-- 조건에 맞는 사용자와 총 거래금액 조회하기     ->WHERE절에 테이블 별칭사용O / 컬럼의 별칭사용X
+SELECT U.USER_ID, U.NICKNAME, SUM(B.PRICE) AS TOTAL_SALES
+FROM USED_GOODS_USER U INNER JOIN USED_GOODS_BOARD B ON U.USER_ID = B.WRITER_ID
+WHERE B.STATUS = 'DONE'
+GROUP BY U.USER_ID
+HAVING TOTAL_SALES >= 700000
+ORDER BY TOTAL_SALES;
+
+-- 다중열 group by
+-- group by 첫번째컬럼, 두번째컬럼 : 첫번째컬럼으로 먼저 grouping이후에 두번째컬럼으로 grouping
+-- post테이블에서 작성자별로 작성한 제목의 개수를 출력하시오
+select author_id, title, count(*) from post group by author_id, title;
+-- 재구매가 일어난 상품과 회원 리스트 구하기
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE 
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) >= 2                        -- -> 1, 2번째 그룹핑한 컬럼의 count임
+ORDER BY USER_ID, PRODUCT_ID DESC;
+
+*셀프조인왜글행오?
+
+
+
---
+++ 9.index.sql
@@ -0,0 +1,54 @@
+-- pk, fk, unique 제약조건 추가시에 해당컬럼에 대해 index페이지 자동생성
+-- index가 만들어지면, 조회성능은 향상. 추가/수정/삭제 성능 하락.
+
+-- index 조회
+show index from author;
+
+-- index 삭제
+alter table author drop index 인덱스명;
+
+-- index 생성
+create index 인덱스명 on 테이블(컬럼명);
+
+-- index를 통해 조회성능향상을 얻으려면, 반드시 where조건에 해당컬럼에 대한 조건이 있어야함.
+select * from author where email="hongildong@naver.com";
+
+-- 만약 where조건에서 2컬럼으로 조회시에, 1컬럼에만 index가 있다면 (-> index컬럼 먼저 조회하고 그 중에서 나머지 컬럼 조회)
+select * from author where name='hong' and email='hongildong@naver.com';
+
+-- 만약 where조건에서 2컬럼으로 조회시에, 2컬럼 모두에 index가 있다면
+-- 이 경우 DB엔진에서 최적의 알고리즘 실행.
+select * from author where name='hong' and email='hongildong@naver.com';
+
+-- index는 1컬럼 뿐만아니라, 2컬럼을 대상으로 1개의 index를 설정하는 것도 가능.
+-- 이 경우 두컬럼을 and조건으로 조회해야만 index를 사용한다.
+-- 복합 인덱스 생성
+create index 인덱스명 on 테이블명(컬럼1, 컬럼2);
+
+-- 기존테이블 삭제 후 아래 테이블로 신규생성
+create table author(id bigint auto_increment, email varchar(255), name varchar(255), primary key(id));
+
+-- index테스트 시나리오
+-- 아래 프로시저를 통해 수십만건의 데이터 insert후에 index생성 전후에 따라 조회성능확인인
+DELIMITER //
+CREATE PROCEDURE insert_authors()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE email VARCHAR(100);
+    DECLARE batch_size INT DEFAULT 10000; -- 한 번에 삽입할 행 수
+    DECLARE max_iterations INT DEFAULT 100; -- 총 반복 횟수 (1000000 / batch_size)
+    DECLARE iteration INT DEFAULT 1;
+    WHILE iteration <= max_iterations DO
+        START TRANSACTION;
+        WHILE i <= iteration * batch_size DO
+            SET email = CONCAT('seonguk', i, '@naver.com');
+            INSERT INTO author (email) VALUES (email);
+            SET i = i + 1;
+        END WHILE;
+        COMMIT;
+        SET iteration = iteration + 1;
+        DO SLEEP(0.1); -- 각 트랜잭션 후 0.1초 지연
+    END WHILE;
+END //
+DELIMITER ;
+
